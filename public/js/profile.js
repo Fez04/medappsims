404f5787ee84fea0a7d25bb6a02646ca245831e0
david <david_avendano@hotmail.com>
2018-04-04 22:44:01 -0700

    done
    done

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,38 +1,46 @@
 //Let's the user add times
-console.log("it works");
 var addTimeForm = document.forms.addTimeForm;
 
 addTimeForm.addEventListener("submit", function(e) {
     addTime(e);
 });
 
-var timeArrays = [];
+// var timeArrays = [];
 
 function addTime(e) {
     e.preventDefault();
     var timeUl = document.querySelector("#time-list");
-    var timeInput = document.querySelector("#medTime").value;
-    var clearTime = document.querySelector("#medTime").value = "";
+    // var timeInput = document.querySelector("#medTime").value;
+    // var medInput = document.querySelector("#addMed").value;
+    // var timeMeds = document.querySelector("#joint").value = medTime + addMed;
+    // var clearTime = document.querySelectorAll("#medTime").value = "";
+    var timeMeds = {
+        timeInput: document.getElementById('medTime').value,
+        medInput: document.getElementById('addMed').value
+    };
+    var test= document.getElementById('medTime').value +" "+ document.getElementById('addMed').value
+    console.log(timeMeds);
 
     var timeLi = document.createElement("li");
     var timeForm = document.createElement("form");
     var timeBtn = document.createElement("button");
     timeBtn.classList.add("btn", "btn-block", "med-list-button");
     timeBtn.setAttribute("type", "submit",);
-    timeBtn.setAttribute("value", timeInput);
-    timeForm.setAttribute("action", "users/submit");
+    timeBtn.setAttribute("value", test);
+    timeBtn.setAttribute("id", "timeBtn");
+    timeForm.setAttribute("action", "/testtwilio");
     timeForm.setAttribute("method", "post");
 
-    if (timeInput === "") {
-        alert("Oops, please type in a time.")
-    } else {
-        timeUl.appendChild(timeLi);
-    }
+    // if (timeInput === "") {
+    //     alert("Oops, please type in a time.")
+    // } else {
+    //     timeUl.appendChild(timeLi);
+    // }
 
-    timeArrays.push(timeInput);
+    // timeArrays.push(timeMeds);
 
+    timeUl.appendChild(timeLi);
     timeLi.appendChild(timeForm);
     timeForm.appendChild(timeBtn);
-    timeBtn.innerHTML = timeInput;
-
+    timeBtn.innerHTML = test;
 }
